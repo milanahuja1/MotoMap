faa401041ef540cf27fac1560da8ab255fd87dcd
Milan Ahuja <mahu925@aucklanduni.ac.nz>
2026-01-21 12:20:22 +1300

    Added specific permission for iOS and Android.

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,11 +1,14 @@
-import Mapbox, { MapView } from "@rnmapbox/maps";
+import Mapbox, { Camera, LocationPuck, MapView } from "@rnmapbox/maps";
 import { StyleSheet, View } from 'react-native';
 Mapbox.setAccessToken("pk.eyJ1IjoibWlsYW5haHVqYSIsImEiOiJjbWttOHpneWowZHB6M2Nvdm1keDczZjk1In0.KGzKZ1ywfzsMES3djPQRLw");
 Mapbox.setTelemetryEnabled(false);
 export default function HomeScreen() {
 return (
 <View style={styles.container}>
-      <MapView style={StyleSheet.absoluteFillObject} />
+      <MapView style={StyleSheet.absoluteFillObject}>
+          <Camera zoomLevel={14} followUserLocation={true} />
+          <LocationPuck puckBearingEnabled={true}/>
+      </MapView>
     </View>
       
   );
