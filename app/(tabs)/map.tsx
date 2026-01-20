2455fc4db142e7c8e2bef50b468099f59e44e751
Milan Ahuja <mahu925@aucklanduni.ac.nz>
2026-01-21 12:05:25 +1300

    Made the map full screen and added location permissions.

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,45 +1,18 @@
 import Mapbox, { MapView } from "@rnmapbox/maps";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 Mapbox.setAccessToken("pk.eyJ1IjoibWlsYW5haHVqYSIsImEiOiJjbWttOHpneWowZHB6M2Nvdm1keDczZjk1In0.KGzKZ1ywfzsMES3djPQRLw");
 Mapbox.setTelemetryEnabled(false);
 export default function HomeScreen() {
 return (
-    <View style={{ flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-     }}>
-      <Text>Map Screen</Text>
-              <View style={styles.container}>
-          <MapView style={styles.map} />
-        </View>
+<View style={styles.container}>
+      <MapView style={StyleSheet.absoluteFillObject} />
     </View>
       
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-  map: {
-    flex: 1
-  },
-  container: {
-    height: 300,
-    width: 300,
-    backgroundColor: "tomato"
+    container: {
+    flex: 1,
   }
 });
