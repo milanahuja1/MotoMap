0525b57cb3b35222ef81a3063a07c015f77986c5
Milan Ahuja <mahu925@aucklanduni.ac.nz>
2026-01-21 18:52:04 +1300

    Refactored the map and the search bar into their own functions

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,31 +1,49 @@
 import Mapbox, { Camera, LocationPuck, MapView } from "@rnmapbox/maps";
-import { useState } from "react";
+import React from "react";
 import { StyleSheet, TextInput, View } from 'react-native';
+
 Mapbox.setAccessToken("pk.eyJ1IjoibWlsYW5haHVqYSIsImEiOiJjbWttOHpneWowZHB6M2Nvdm1keDczZjk1In0.KGzKZ1ywfzsMES3djPQRLw");
 Mapbox.setTelemetryEnabled(false);
-export default function HomeScreen() {
-  const [query, setQuery] = useState('');
+
+
+
+
+export default function MapScreen() {
+  const [query, onChangeText] = React.useState('');
 return (
 <View style={styles.container}>
-      <MapView style={StyleSheet.absoluteFillObject}>
-          <Camera zoomLevel={14} followUserLocation={true} />
-          <LocationPuck puckBearingEnabled={true}/>
-      </MapView>
-      
+      <Map />
       <View style={styles.searchContainer}>
-        <TextInput
-          placeholder="Search"
-          value={query}
-          onChangeText={setQuery}
-            onSubmitEditing={() => {
-    console.log('User searched:', query);
-  }}
-          style={styles.searchBox}
-          />
+        <SearchBar value={query} onChangeText={onChangeText} />
       </View>
     </View>
   );
 }
+function Map(){
+  return(
+<MapView style={StyleSheet.absoluteFillObject}>
+          <Camera zoomLevel={14} followUserLocation={true} />
+          <LocationPuck puckBearingEnabled={true}/>
+      </MapView>
+  );
+}
+function SearchBar({
+  value,
+  onChangeText,
+}: {
+  value: string;
+  onChangeText: (text: string) => void;
+}) {
+  return (
+    <TextInput
+      placeholder="Search for a destination"
+      style={styles.searchBox}
+      value={value}
+      onChangeText={onChangeText}
+    />
+  );
+}
+
 
 const styles = StyleSheet.create({
     container: {
