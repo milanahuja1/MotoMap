bad648aa4624a89608ad4a18e7953be19d1a4e27
Milan Ahuja <mahu925@aucklanduni.ac.nz>
2026-01-22 12:56:43 +1300

    App will display a list of the items received from the API.

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,6 +1,6 @@
 import Mapbox, { Camera, LocationPuck, MapView } from "@rnmapbox/maps";
-import React, { useState } from "react";
-import { StyleSheet, TextInput, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 Mapbox.setAccessToken("pk.eyJ1IjoibWlsYW5haHVqYSIsImEiOiJjbWttOHpneWowZHB6M2Nvdm1keDczZjk1In0.KGzKZ1ywfzsMES3djPQRLw");
 Mapbox.setTelemetryEnabled(false);
@@ -11,6 +11,16 @@ const API_ENDPOINT = `https://randomuser.me/api/?results=30`;
 export default function MapScreen() {
   const [query, setQuery] = useState('');
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [fullData, setFullData] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchData(API_ENDPOINT);
+    setIsLoading(false);
+  }, []);
+
     const handleSearchChange = (text: string) => {
     setQuery(text);
     fetchData(API_ENDPOINT)
@@ -28,13 +38,35 @@ export default function MapScreen() {
       console.log(error);
     }};
 
+    if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator size={"large"} color="blue" />
+      </View>
+      );}
+
 return (
 <View style={styles.container}>
       <Map />
       <View style={styles.searchContainer}>
         <SearchBar value={query} onQuery={handleSearchChange} />
+        <SearchList data={data} query={query} />
       </View>
     </View>
+  );
+}
+function SearchList({ data }: { data: any[] }) {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.login.username}
+      renderItem={({ item }) => (
+        <View>
+          <Text>{item.name.first} {item.name.last}</Text>
+          <Text>{item.email}</Text>
+        </View>
+      )}
+    />
   );
 }
 function Map(){
